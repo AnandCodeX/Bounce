a6f80e59532a40ff65da1ef37bc0c8a34d8e85e5
AnandCodeX <Anandrtiwari@live.com>
2023-11-22 13:06:19 +0000

    bounce

--- Frontend/src/components/api/getCountryData.js
+++ Frontend/src/components/api/getCountryData.js
@@ -4,8 +4,6 @@ import axios from "axios";
 
 //Fetching Data From Backend API
 export const getCountryData = async (country) => {
-	const response = await axios.get(
-		`https://bounsebackend-production.up.railway.app/country/${country}`
-	);
+	const response = await axios.get(`http://localhost:6000/country/${country}`);
 	return response;
 };
